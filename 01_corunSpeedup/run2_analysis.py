8e195bec1039625ba2369b48ac5813996dc9caf0
Leiming Yu <leimingyu830@gmail.com>
2018-11-14 14:07:48 -0500

    run2 analysis draft

---
+++ 01_corunSpeedup/run2_analysis.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import os,sys
+import numpy as np
+
+sys.path.append('../prepare')
+from app_info import *  # app and app2dir_dd
+
+#sys.path.append('../utils')
+#from magic_common import cd
+
+
+folders = ['./homedesktop-gpu0', './homedesktop-gpu1', 
+        './hoyi-gpu0', './hoyi-gpu1']
+
+def main():
+
+    print("Total GPU applications = {}".format(len(app)))
+
+    for fold in folders:
+        print fold
+        for name in app:
+            # step1: read dedicated runtime for the app
+            dedicate_rt = 1.
+
+            # step2: read the perf for run2 cases
+            fullpath = str(fold) + "/" + str(name) + ".npy"
+            print fullpath
+            
+            app_run2_dd = np.load(fullpath).item()
+
+            # step3: compute the slowdown for all the combinations
+            slowdown_list = []
+            for key, corun_rt in app_run2_dd.iteritems():
+                #print key, corun_rt 
+                slowdown = dedicate_rt / corun_rt 
+                slowdown_list.append(slowdown)
+
+            # step4: compute avg slowdown
+            avg_slowdown = sum(slowdown_list) / len(slowdown_list)
+            print name, avg_slowdown
+
+            break
+        break
+        
+
+
+
+if __name__ == "__main__":
+    main()
